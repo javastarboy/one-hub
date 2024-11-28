df4e3f9c85ef713775523becf27828a1679ad249
liumingxing <594086597@qq.com>
2024-11-28 17:44:30 +0800

    Update baseIndex.jsx

--- web/src/views/Home/baseIndex.jsx
+++ web/src/views/Home/baseIndex.jsx
@@ -18,27 +18,27 @@ const BaseIndex = () => {
     >
       <Container maxWidth="lg">
         <Grid container columns={9} wrap="nowrap" alignItems="flex-start" sx={{ minHeight: 'calc(100vh - 230px)', paddingTop: '3vh' }}>
-          <Grid md={7} lg={6} sx={{ marginLeft: '-5%' }}>
+          <Grid md={7} lg={6} sx={{ marginLeft: '-20%', marginTop: '8vh' }}>
             <Stack>
-                <Typography variant="h1" sx={{ fontSize: '4rem', color: '#fff', lineHeight: 1.5 }}>
+                <Typography variant="h1" sx={{ fontSize: '4rem', color: '#800080', lineHeight: 2.5 }}>
                 深渊智能AIGC
               </Typography>
-              <Typography variant="h4" sx={{ fontSize: '1.5rem', color: '#fff', lineHeight: 1.5, marginBottom: '5vh' }}>
+              <Typography variant="h4" sx={{ fontSize: '1.5rem', color: '#800080', lineHeight: 1.5, marginBottom: '5vh' }}>
                 {t('description')}
               </Typography>
             </Stack>
-            <Typography variant="h4" sx={{ fontSize: '1.4rem', color: '#ffff00', lineHeight: 1.5, marginBottom: '5vh' }}>
+            <Typography variant="h4" sx={{ fontSize: '1.4rem', color: '#800080', lineHeight: 1, marginBottom: '5vh' }}>
                 {t('携手深渊智能AIGC，聚合多种模型，驶入前沿科技，让AI助你一臂之力！')}
             </Typography>
             
-            <Stack spacing={3} direction="row" sx={{ marginTop: '40vh' }}>
+            <Stack spacing={3} direction="row" sx={{ marginTop: '30vh' }}>
               <Button
-                  startIcon={<Link />}
-                  variant="contained"
-                  href="https://www.yuque.com/shenyuanzhinengaigc/atz69s/connectus" target='_blank'
-                  sx={{ backgroundColor: '#24292e', color: '#fff', width: 'fit-content', boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)' }}
-                >
-                  联系我们
+                startIcon={<Class />}
+                variant="contained"
+                href="https://www.yuque.com/shenyuanzhinengaigc/atz69s/uabc1f5yuyobqcus" target='_blank'
+                sx={{ backgroundColor: '#24292e', color: '#fff', width: 'fit-content', boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)' }}
+              >
+                🎉 国内外AI大语言模型API价格对比
               </Button>
               <Button
                 startIcon={<Class />}
@@ -67,12 +67,12 @@ const BaseIndex = () => {
                 AI大模型全栈通识教程
               </Button>
               <Button
-                startIcon={<Class />}
-                variant="contained"
-                href="https://www.yuque.com/shenyuanzhinengaigc/atz69s/uabc1f5yuyobqcus" target='_blank'
-                sx={{ backgroundColor: '#24292e', color: '#fff', width: 'fit-content', boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)' }}
-              >
-                🎉 国内外AI大语言模型API价格对比
+                  startIcon={<Link />}
+                  variant="contained"
+                  href="https://www.yuque.com/shenyuanzhinengaigc/atz69s/connectus" target='_blank'
+                  sx={{ backgroundColor: '#24292e', color: '#fff', width: 'fit-content', boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)' }}
+                >
+                  联系我们
               </Button>
             </Stack>
           </Grid>
